48eefd6fd63aeb6bd3788dfc0731884fded67fed
Rennan Andres Paloschi <rennan_paloschi@yahoo.com>
2025-12-22 14:06:53 +0000

    trata vazamento de conexões

---
+++ tests/integration/api/v1/migrations/anyother.test.js
@@ -0,0 +1,22 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+test("ANYOTHER to /api/v1/migrations should return 405", async () => {
+  const othermethods = ["PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"];
+
+  for (const method of othermethods) {
+    const response = await fetch("http://localhost:3000/api/v1/migrations", {
+      method: method,
+    });
+
+    // console.log("Status: " + response.status);
+    // console.log("Error: " + (await response.json()).error);
+
+    expect(response.status).toBe(405);
+  }
+});
