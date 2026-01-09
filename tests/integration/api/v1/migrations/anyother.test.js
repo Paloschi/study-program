dd1d434a0fb9c9d57217d00ae70addeb4b17e3f0
Rennan Andres Paloschi <rennan_paloschi@yahoo.com>
2026-01-09 10:51:29 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/anyother.test.js
+++ tests/integration/api/v1/migrations/anyother.test.js
@@ -6,17 +6,20 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("ANYOTHER to /api/v1/migrations should return 405", async () => {
-  const othermethods = ["PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"];
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Any other method", async () => {
+      const othermethods = ["PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"];
 
-  for (const method of othermethods) {
-    const response = await fetch("http://localhost:3000/api/v1/migrations", {
-      method: method,
+      for (const method of othermethods) {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: method,
+          },
+        );
+        expect(response.status).toBe(405);
+      }
     });
-
-    // console.log("Status: " + response.status);
-    // console.log("Error: " + (await response.json()).error);
-
-    expect(response.status).toBe(405);
-  }
+  });
 });
