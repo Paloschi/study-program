63b06a521aa23c7c038e857dc1caea88776d6e4e
Rennan Andres Paloschi <rennan_paloschi@yahoo.com>
2026-01-19 14:08:07 -0300

    feat: create and use `ValidationError` in `controler.js`

--- infra/errors.js
+++ infra/errors.js
@@ -38,6 +38,26 @@ export class ServiceError extends Error {
   }
 }
 
+export class ValidationError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "A validation error occurred.", {
+      cause,
+    });
+    this.name = "ValidationError";
+    this.action = action || "Verify the data and try again.";
+    this.statusCode = 400;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      statusCode: this.statusCode,
+    };
+  }
+}
+
 export class MethodNotAllowedError extends Error {
   constructor() {
     super("Method Not Allowed to this endpoint.");
