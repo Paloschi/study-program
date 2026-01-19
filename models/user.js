55d93420e455a4e05809b94d1f5c2f602868a6e0
Rennan Andres Paloschi <rennan_paloschi@yahoo.com>
2026-01-19 14:09:10 -0300

    feat: create `user` model and `/api/v1/users` endpoint

---
+++ models/user.js
@@ -0,0 +1,75 @@
+import database from "infra/database.js";
+import { ValidationError } from "infra/errors.js";
+
+async function create(userImputValues) {
+  await validateUniqueEmail(userImputValues.email);
+  await validateUniqueUsername(userImputValues.username);
+
+  const newUser = await runInsertQuery(userImputValues);
+  return newUser;
+
+  async function validateUniqueEmail(email) {
+    const results = await database.query({
+      text: `
+        SELECT 
+          email 
+        FROM 
+          users 
+        WHERE 
+          LOWER(email) = LOWER($1)
+      ;`,
+      values: [email],
+    });
+    if (results.rowCount > 0) {
+      throw new ValidationError({
+        message: "The email is already in use.",
+        action: "Try a different email to register",
+      });
+    }
+  }
+
+  async function validateUniqueUsername(username) {
+    const results = await database.query({
+      text: `
+        SELECT 
+          username 
+        FROM 
+          users 
+        WHERE 
+          LOWER(username) = LOWER($1)
+      ;`,
+      values: [username],
+    });
+    if (results.rowCount > 0) {
+      throw new ValidationError({
+        message: "The username is already in use.",
+        action: "Try a different username to register.",
+      });
+    }
+  }
+
+  async function runInsertQuery(userImputValues) {
+    const results = await database.query({
+      text: `
+        INSERT INTO 
+          users (username, email, password) 
+        VALUES 
+          ($1, $2, $3)
+        RETURNING 
+          *
+        ;`,
+      values: [
+        userImputValues.username,
+        userImputValues.email,
+        userImputValues.password,
+      ],
+    });
+    return results.rows[0];
+  }
+}
+
+const user = {
+  create,
+};
+
+export default user;
